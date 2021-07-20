cb33c89edc780caa4f5d99902d18b67040dc96c9
vtiv <vtiv@deakin.edu.au>
2021-07-20 16:23:11 +1000

    Add the reconstruction of the context

--- IncumbentCallback.cs
+++ IncumbentCallback.cs
@@ -14,10 +14,16 @@ namespace _2DWVSBPP_with_Visualizer
     class IncumbentCallback : Cplex.Callback.Function
     {
         private double obj;
+        private IIntVar[][][] x;
+        private List<Item> items;
+        private Instance inst;
 
-        public IncumbentCallback(double obj)
+        public IncumbentCallback(double obj, IIntVar[][][] x, List<Item> items, Instance inst)
         {
             this.obj = obj;
+            this.x = x;
+            this.items = items;
+            this.inst = inst;
         }
         public void Invoke(Context context)
         {
@@ -41,9 +47,24 @@ namespace _2DWVSBPP_with_Visualizer
                 //if it's candidate is better that current best obj value
                 if (context.GetCandidateObjective() < obj)
                 {
+                    
                     //TODO: Check Feasibility
                     Console.WriteLine("callback working");
                 }
+
+                for(int binType = 0; binType < inst.m; binType++)
+                {
+                    for(int bin = 0; bin < inst.n; bin++)
+                    {
+                        List<Item> assignment = new List<Item>();
+                        for(int item = 0; item < inst.n; item++)
+                        {
+                            if ((int)Math.Round(context.GetCandidatePoint(x[item][bin][binType])) == 1) assignment.Add(items[item]);
+                        }
+                    }
+                }
+                
+
             }
             catch (ILOG.Concert.Exception exc)
             {
@@ -52,3 +73,9 @@ namespace _2DWVSBPP_with_Visualizer
         }
     }
 }
+
+/*TODO*/
+/*
+ * recontruct the context
+ * For each bin and its solution
+ */
